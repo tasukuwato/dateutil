47ddbe2462a1ef8b775687657ead3304ae0602f6
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2023-09-24 22:10:02 +0900

    add my_dateutil_2.py

--- my_dateutil_2.py
+++ my_dateutil_2.py
@@ -1,4 +1,14 @@
 import dateutil
 import datetime
-from dateutil.relativedelta import relativedelta
+#timezoneを扱うために「pytz」をimportする
+import pytz
+from dateutil import tz
 
+
+u = dateutil.tz.tzutc()
+print(datetime.datetime.now(u))
+
+#timezoneの処理
+u = dateutil.tz.gettz('JP/Japan')
+#timezoneはwikipediaを参照:https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+print(datetime.datetime.now(u))
